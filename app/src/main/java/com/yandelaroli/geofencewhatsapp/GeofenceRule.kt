27c84c36cec9d2846089fad4d026a3ace46f7a95
Yan Barreto <44248460+YanDelaroli@users.noreply.github.com>
2026-08-09 14:19:35 -0300

    add geofence rule model

---
+++ app/src/main/java/com/yandelaroli/geofencewhatsapp/GeofenceRule.kt
@@ -0,0 +1,12 @@
+package com.yandelaroli.geofencewhatsapp
+
+data class GeofenceRule(
+    val id: String,
+    val name: String,
+    val latitude: Double,
+    val longitude: Double,
+    val radiusMeters: Float,
+    val phone: String,
+    val message: String,
+    val enabled: Boolean = true
+)
